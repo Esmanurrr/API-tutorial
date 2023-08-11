e9d363caa2c69d44cc78d65edffe29bae81cfa1f
zehrahome <zehrakarakaya999@gmail.com>
2023-08-11 23:03:37 +0300

    an attribute is added to control data inputs

--- bsStoreApp/WebAPI/Models/Book.cs
+++ bsStoreApp/WebAPI/Models/Book.cs
@@ -2,8 +2,11 @@
 {
     public class Book
     {
+        [RequiredControl]
         public int Id { get; set; }
+        [RequiredControl]
         public String Title { get; set; }
+        [RequiredControl]
         public decimal Price { get; set; }
     }
 }
